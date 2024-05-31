28305ed78f1dde8ca1356e6b9368996c16c3fdf1
Guilherme Carmo Tavares <161094582+GuilhermeCTavares@users.noreply.github.com>
2024-05-31 16:03:06 -0300

    Update 7.c

--- vetores/7.c
+++ vetores/7.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
 int main() {
-  int vetorA[10], vetorB[10], i;
+  int vetor[15], i, j, aux;
 
-  printf("Digite 10 números inteiros para o vetor A:\n");
-  for (i = 0; i < 10; i++) {
+  printf("Digite 15 números inteiros:\n");
+  for (i = 0; i < 15; i++) {
     printf("Número %d: ", i + 1);
-    scanf("%d", &vetorA[i]);
+    scanf("%d", &vetor[i]);
   }
 
-  for (i = 0; i < 10; i++) {
-    vetorB[i] = vetorA[i];
+  for (i = 0; i < 14; i++) {
+    for (j = 0; j < 14 - i; j++) {
+      if (vetor[j] > vetor[j + 1]) {
+        aux = vetor[j];
+        vetor[j] = vetor[j + 1];
+        vetor[j + 1] = aux;
+      }
+    }
   }
 
-  printf("\nVetor B após a cópia:\n");
-  for (i = 0; i < 10; i++) {
-    printf("%d ", vetorB[i]);
+  printf("\nVetor ordenado:\n");
+  for (i = 0; i < 15; i++) {
+    printf("%d ", vetor[i]);
   }
 
   return 0;
-}\
+}
